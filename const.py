16ff3e3bfeb9501878c91b2bafd744eec4baf169
DengineerPC <GitHub@dengineerpc.com.au>
2025-12-22 20:52:35 +1100

    Add HTTPS support constants (CONF_PORT, CONF_VERIFY_SSL, DEFAULT_PORT, DEFAULT_VERIFY_SSL)

---
+++ const.py
@@ -0,0 +1,9 @@
+"""Constants for the SolPlanet Dengineerpc integration."""
+
+# Configuration keys
+CONF_PORT = "port"
+CONF_VERIFY_SSL = "verify_ssl"
+
+# Default values
+DEFAULT_PORT = 8080
+DEFAULT_VERIFY_SSL = True
